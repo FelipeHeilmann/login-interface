63cc608e59218161892f56b2193cca09b891d914
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-15 20:52:06 -0300

    conexao com mailtrap e template de email

---
+++ server/src/modules/mailer.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer'
+import hbs from 'nodemailer-express-handlebars'
+import path from 'path'
+
+//acessando a plataforma mailtrap para envio de emails
+const transport = nodemailer.createTransport({ 
+    host: process.env.MAILER_HOST,
+    port: process.env.MAILER_PORT,
+    auth: {
+      user: process.env.MAILER_USER,
+      pass: process.env.MAILER_PASS
+    }
+  });
+
+//configuracao de template de email com nodemailer
+transport.use('compile', hbs({
+    viewEngine: 'handlebars',
+    viewPath: path('./src/resources/mail/'),
+    extName: ".html",
+
+  }))
+
+module.exports = transport
