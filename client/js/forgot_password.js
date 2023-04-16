af079c96654a67eb01b7baefea58614e9cc14bde
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-16 00:40:43 -0300

    validacao nos campos de forgot e reset password

--- client/js/forgot_password.js
+++ client/js/forgot_password.js
@@ -4,6 +4,12 @@ const message = document.querySelector('h3')
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
+    email.classList.remove('wrong-input')
+    if(email.value === ''){
+        email.classList.add('wrong-input')
+        message.style.color = 'red'
+        message.innerText = 'The field can not be empty'
+    }
     const userEmail = {email: email.value}
     axios.post('http://localhost:3000/forgot_password', userEmail)
     .then(res=>{
--- client/js/reset_password.js
+++ client/js/reset_password.js
@@ -6,6 +6,14 @@ const token = queryString.split('=')[1]
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
+    inputs.forEach(input=>{
+        input.classList.remove('wrong-input')
+        if(input.value === ''){
+            input.classList.add('wrong-input')
+            message.style.color = 'red'
+            message.innerText = 'The fields can not be empty'
+        }
+    })
     const formData = new FormData(form)//objeto que pega os dados do form 
     const data = Object.fromEntries(formData, token)//converte os dados em um objeto javascript
     data.token = token
